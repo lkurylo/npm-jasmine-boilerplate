468462e2c5eaff2de74a502e2b9ab3fd8eaf33d9
Łukasz Kuryło <secret@gmail.com>
2017-01-17 14:53:13 +0100

    added sample tests

---
+++ spec/tests.spec.js
@@ -0,0 +1,9 @@
+describe('sample initial tests', function () {
+    it('valid test', function () {
+        expect(true).toBe(true);
+    })
+
+    it('invalid test', function () {
+        expect(true).toBe(false);
+    });
+});
